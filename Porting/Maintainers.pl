a3319b99838ced745db2a3f0a0b19ea7d48510b8
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-12-04 17:53:13 +0100

    cpan/parent - Update to version 0.243
    0.243 2024-11-27
        . Reinstate test for apostrophe as package separator, as the package
          separator is allowed again
        . No code change, only tests have been amended
    
    co-authored-by: Eric Herman <eric@freesa.org>
    co-authored-by: Philippe Bruhat (BooK) <book@cpan.org>

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -885,8 +885,8 @@ our %Modules = (
     },
 
     'parent' => {
-        'DISTRIBUTION' => 'CORION/parent-0.242.tar.gz',
-        'SYNCINFO'     => 'jkeenan on Wed Aug 14 21:41:51 2024',
+        'DISTRIBUTION' => 'CORION/parent-0.243.tar.gz',
+        'SYNCINFO'     => 'tib on Wed Dec  4 17:52:22 2024',
         'FILES'        => q[cpan/parent],
         'EXCLUDED'     => [
             qr{^xt}
--- cpan/parent/lib/parent.pm
+++ cpan/parent/lib/parent.pm
@@ -1,7 +1,7 @@
 package parent;
 use strict;
 
-our $VERSION = '0.242_001';
+our $VERSION = '0.243';
 
 sub import {
     my $class = shift;
