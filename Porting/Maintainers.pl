6f836886eca09c5df3a9f985231636e29b8f10dc
Karen Etheridge <ether@cpan.org>
2024-12-16 12:21:31 -0800

    Update CPAN::Meta::YAML to 0.020

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -329,8 +329,8 @@ our %Modules = (
     },
 
     'CPAN::Meta::YAML' => {
-        'DISTRIBUTION' => 'ETHER/CPAN-Meta-YAML-0.019.tar.gz',
-        'SYNCINFO'     => 'corion on Mon Dec 16 09:12:25 2024',
+        'DISTRIBUTION' => 'ETHER/CPAN-Meta-YAML-0.020.tar.gz',
+        'SYNCINFO'     => 'ether on Mon Dec 16 12:20:28 2024',
         'FILES'        => q[cpan/CPAN-Meta-YAML],
         'EXCLUDED'     => [
             't/00-report-prereqs.t',
